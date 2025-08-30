f64b387bd682d1caafa8f74b8296848934e46085
Amira Figueroa Attiyeh <amirafigueroaattiyeh@Amiras-MacBook-Air.local>
2025-08-30 10:43:52 -0400

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import GridPortfolio from "./grid/page";
+
+export default function Home() {
+  return (
+<div>
+      <GridPortfolio />
+</div>
+  );
+}
---
+++ src/data/projectData.js
@@ -0,0 +1,32 @@
+// src/data/projectData.js
+export const projects = [
+  {
+    id: 1,
+    title: {
+      en: "Hatsune Miku: Biography",
+      es: "Biografía de Hatsune Miku",
+    },
+    description: {
+      en: "Biography of the virtual singer Hatsune Miku, created using HTML and CSS.",
+      es: " Biografía de la cantante virtual Hatsune Miku, creada con HTML y CSS.",
+    },
+    image: "",
+    liveSite: "https://11amiraf.github.io/Hatsune-Miku-Biography/",
+    githubRepo: "https://github.com/11AmiraF/Hatsune-Miku-Biography.git",
+  },
+  {
+    id: 2,
+    title: {
+      en: "Sharing Boxes of Calm",
+      es: "Compartiendo Cajitas De La Calma",
+    },
+    description: {
+      en: "A project aimed at promoting mental health and well-being through calming boxes.",
+      es: "Un proyecto destinado a promover la salud mental y el bienestar a través de cajitas de la calma.",
+    },
+    image: "",
+    liveSite: "https://11amiraf.github.io/Compartiendo-Cajitas-De-La-Calma/",
+    githubRepo: "https://reimagined-doodle-q7x7wjg9jq49fxrww.github.dev/",
+  },
+
+];
